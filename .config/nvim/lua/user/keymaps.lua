a968fe137ba57250354c95c3e7e92f78a4d29732
bechirMakhlouf <bechirM@proton.me>
2024-02-02 17:10:17 +0100

    add lsp initial config

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,20 +1,17 @@
 local nnoremap = require("user.keymap_utils").nnoremap
--- local vnoremap = require("user.keymap_utils").vnoremap
--- local inoremap = require("user.keymap_utils").inoremap
--- local tnoremap = require("user.keymap_utils").tnoremap
--- local xnoremap = require("user.keymap_utils").xnoremap
+local vnoremap = require("user.keymap_utils").vnoremap
+local inoremap = require("user.keymap_utils").inoremap
+local tnoremap = require("user.keymap_utils").tnoremap
+local xnoremap = require("user.keymap_utils").xnoremap
+
+local TERM = os.getenv("TERM")
+
+
 
 -- Normal --
 
 -- Disable Space bar since it'll be used as the leader key
 nnoremap("<space>", "<nop>")
-
-
-
--- oil
-nnoremap("<leader>e", function()
-	require("oil").toggle_float()
-end)
 
 -- Center buffer while navigating
 nnoremap("<C-u>", "<C-u>zz")
@@ -30,3 +27,92 @@ nnoremap("<C-o>", "<C-o>zz")
 nnoremap("%", "%zz")
 nnoremap("*", "*zz")
 nnoremap("#", "#zz")
+
+-- Diagnostics
+
+-- Goto next diagnostic of any severity
+nnoremap("]d", function()
+	vim.diagnostic.goto_next({})
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous diagnostic of any severity
+nnoremap("[d", function()
+	vim.diagnostic.goto_prev({})
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto next error diagnostic
+nnoremap("]e", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous error diagnostic
+nnoremap("[e", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto next warning diagnostic
+nnoremap("]w", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous warning diagnostic
+nnoremap("[w", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+
+-- oil
+nnoremap("<leader>e", function()
+	require("oil").toggle_float()
+end)
+
+local M = {}
+-- LSP Keybinds (exports a function to be used in ../../after/plugin/lsp.lua b/c we need a reference to the current buffer) --
+M.map_lsp_keybinds = function(buffer_number)
+	nnoremap("<leader>rn", vim.lsp.buf.rename, { desc = "LSP: [R]e[n]ame", buffer = buffer_number })
+	nnoremap("<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: [C]ode [A]ction", buffer = buffer_number })
+
+	nnoremap("gd", vim.lsp.buf.definition, { desc = "LSP: [G]oto [D]efinition", buffer = buffer_number })
+
+	-- Telescope LSP keybinds --
+	-- nnoremap(
+	-- 	"gr",
+	-- 	require("telescope.builtin").lsp_references,
+	-- 	{ desc = "LSP: [G]oto [R]eferences", buffer = buffer_number }
+	-- )
+
+	-- nnoremap(
+	-- 	"gi",
+	-- 	require("telescope.builtin").lsp_implementations,
+	-- 	{ desc = "LSP: [G]oto [I]mplementation", buffer = buffer_number }
+	-- )
+
+	-- nnoremap(
+	-- 	"<leader>bs",
+	-- 	require("telescope.builtin").lsp_document_symbols,
+	-- 	{ desc = "LSP: [B]uffer [S]ymbols", buffer = buffer_number }
+	-- )
+
+	-- nnoremap(
+	-- 	"<leader>ps",
+	-- 	require("telescope.builtin").lsp_workspace_symbols,
+	-- 	{ desc = "LSP: [P]roject [S]ymbols", buffer = buffer_number }
+	-- )
+
+	-- See `:help K` for why this keymap
+	nnoremap("K", vim.lsp.buf.hover, { desc = "LSP: Hover Documentation", buffer = buffer_number })
+	nnoremap("<leader>k", vim.lsp.buf.signature_help, { desc = "LSP: Signature Documentation", buffer = buffer_number })
+	inoremap("<C-k>", vim.lsp.buf.signature_help, { desc = "LSP: Signature Documentation", buffer = buffer_number })
+
+	-- Lesser used LSP functionality
+	nnoremap("gD", vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration", buffer = buffer_number })
+	nnoremap("td", vim.lsp.buf.type_definition, { desc = "LSP: [T]ype [D]efinition", buffer = buffer_number })
+end
+
+return M
