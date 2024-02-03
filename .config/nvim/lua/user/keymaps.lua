e9ca866dd5c1d149b89640fa0f6dbcd10ed2948e
bechirMakhlouf <bechirM@proton.me>
2024-02-03 11:54:25 +0100

    add key bind to check git commits within telescope

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -192,6 +192,26 @@ nnoremap("<leader>gf", function()
 		require("telescope.builtin").git_files()
 	end
 end, { desc = "Search [G]it [F]iles" })
+
+nnoremap("<leader>gc", function()
+	local cmd = {
+		"sort",
+		"-u",
+		"<(git diff --name-only --cached)",
+		"<(git diff --name-only)",
+		"<(git diff --name-only --diff-filter=U)",
+	}
+
+	if not utils.is_git_directory() then
+		vim.notify(
+			"Current project is not a git directory",
+			vim.log.levels.WARN,
+			{ title = "Telescope Git Files", git_command = cmd }
+		)
+	else
+		require("telescope.builtin").git_commits()
+	end
+end, { desc = "Search [G]it [C]ommits" })
 --
 -- LSP Keybinds (exports a function to be used in ../../after/plugin/lsp.lua b/c we need a reference to the current buffer) --
 local M = {}
