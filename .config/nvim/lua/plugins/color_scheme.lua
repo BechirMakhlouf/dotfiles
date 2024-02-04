1da80b3a037a10eb652f7a805fd0ead76c095a03
bechirMakhlouf <bechirM@proton.me>
2024-02-04 09:55:14 +0100

    add tokyo night colorscheme for nvim

--- .config/nvim/lua/plugins/color_scheme.lua
+++ .config/nvim/lua/plugins/color_scheme.lua
@@ -1,37 +1,81 @@
 return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function ()
+      require("tokyonight").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        light_style = "day", -- The theme is used when the background is set to light
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
-	{
-		"catppuccin/nvim",
-		config = function()
-			require("catppuccin").setup({
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					harpoon = true,
-					illuminate = true,
-					indent_blankline = {
-						enabled = false,
-						scope_color = "sapphire",
-						colored_indent_levels = false,
-					},
-					mason = true,
-					native_lsp = { enabled = true },
-					notify = true,
-					nvimtree = true,
-					neotree = true,
-					symbols_outline = true,
-					telescope = true,
-					treesitter = true,
-					treesitter_context = true,
-				},
-			})
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors) end,
 
-			vim.cmd.colorscheme("catppuccin")
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
+      })
 
-			-- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
-			for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-				vim.api.nvim_set_hl(0, group, {})
-			end
-		end,
-	},
+      vim.cmd.colorscheme("tokyonight")
+    end
+  },
+  -- {
+  --   "catppuccin/nvim",
+  --   config = function()
+  --     require("catppuccin").setup({
+  --       integrations = {
+  --         cmp = true,
+  --         gitsigns = true,
+  --         harpoon = true,
+  --         illuminate = true,
+  --         indent_blankline = {
+  --           enabled = false,
+  --           scope_color = "sapphire",
+  --           colored_indent_levels = false,
+  --         },
+  --         mason = true,
+  --         native_lsp = { enabled = true },
+  --         notify = true,
+  --         nvimtree = true,
+  --         neotree = true,
+  --         symbols_outline = true,
+  --         telescope = true,
+  --         treesitter = true,
+  --         treesitter_context = true,
+  --       },
+  --     })
+  --
+  --     vim.cmd.colorscheme("catppuccin")
+  --
+  --     -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+  --     for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+  --       vim.api.nvim_set_hl(0, group, {})
+  --     end
+  --   end,
+  -- },
 }
